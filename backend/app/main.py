a7263cb4140ad7e100291932f2ead753b2294e42
ljtruong <leon.truong1@gmail.com>
2026-02-19 22:56:13 +1100

    fix backend port

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "*"],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["Origin", "Content-Type"],
